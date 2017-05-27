d0e96530ded8ad78a2e2e675cd276e017728d2b4
vogelino <contact@vogelino.com>
2017-05-27 23:05:02 +0200

    Add sidebar component

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import EventsList from '../components/EventsList';
+import Sidebar from '../components/Sidebar';
 
 const App = () => (
 	<div className="app-container">
+		<Sidebar />
 		<EventsList />
 	</div>
 );
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+// import { combineCssClasses } from '../../helpers/styleHelper';
+import './Sidebar.css';
+
+const SidebarContent = ({
+	id,
+	data: {
+		title,
+		text,
+	},
+}) => (
+	<div className="siderbar_content" id={`sidebar_content-${id}`}>
+		<h1>{title}</h1>
+		<h2>{text}</h2>
+	</div>
+);
+
+SidebarContent.propTypes = {
+	id: PropTypes.string.isRequired,
+	data: PropTypes.shape({
+		title: PropTypes.string.isRequired,
+		text: PropTypes.string.isRequired,
+	}).isRequired,
+};
+
+const SelectMessage = () => (
+	<div className="sidebar_selectMessage">
+		Please select an item to view its content
+	</div>
+);
+
+const Sidebar = ({ selectedEvent }) => (
+	<div className="sidebar">
+		{!selectedEvent ?
+			<SelectMessage /> : <SidebarContent {...selectedEvent} />}
+	</div>
+);
+
+Sidebar.defaultProps = {
+	selectedEvent: null,
+};
+
+Sidebar.propTypes = {
+	selectedEvent: PropTypes.oneOfType([
+		PropTypes.oneOf([null]),
+		PropTypes.shape({
+			id: PropTypes.string.isRequired,
+			data: PropTypes.shape({
+				title: PropTypes.string.isRequired,
+				text: PropTypes.string.isRequired,
+			}).isRequired,
+		}),
+	]),
+};
+
+const mapStateToProps = ({ events }) => ({
+	selectedEvent: events.find(({ state }) => state.selected),
+});
+
+export default connect(mapStateToProps)(Sidebar);
