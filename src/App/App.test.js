9417df27b2bcae1a5a29437ebf5d5f351561f2db
vogelino <contact@vogelino.com>
2017-05-27 14:08:59 +0200

    Add eslint

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,6 +1,7 @@
+/* global it */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './index.js';
+import App from './index';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
