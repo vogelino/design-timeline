7492a1890a216e2cc72a57d7736932c490c21461
vogelino <contact@vogelino.com>
2017-06-01 10:08:10 +0200

    Make sure events have the right keys and an id

--- src/redux/state/fakeEvents.test.js
+++ src/redux/state/fakeEvents.test.js
@@ -7,3 +7,16 @@ test('calling fakeEvents should generate an array containing btw. 60 and 120 obj
 	expect(fakeEvents.length)
 		.toBeLessThanOrEqual(120);
 });
+
+test('events generated with fakeEvents should include 3 major keys: id, data & state', () => {
+	fakeEvents.forEach((event) =>
+		expect(Object.keys(event)).toEqual(['id', 'data', 'state'])
+	);
+});
+
+test('all events should have a string id', () => {
+	fakeEvents.forEach((event) => {
+		expect(event.id).not.toBeUndefined();
+		expect(typeof event.id).toBe('string');
+	});
+});
