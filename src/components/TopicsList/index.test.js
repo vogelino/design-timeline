6719c81322b9021fe76d8a13a371a87c60c4a927
vogelino <contact@vogelino.com>
2017-06-25 01:09:15 +0200

    Fix broken TopicsList test

--- src/components/TopicsList/index.test.js
+++ src/components/TopicsList/index.test.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { TopicsListComponent as TopicsList } from './index';
+import { initialCategories } from '../../redux/reducers/categoriesReducer';
 
 it('should render without crashing', () => {
-	shallow(<TopicsList />);
+	shallow(<TopicsList categories={initialCategories} />);
 });
