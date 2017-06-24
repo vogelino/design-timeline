53292b0f7d49f4eeb60b3f3f8671a098dd430b5f
vogelino <contact@vogelino.com>
2017-06-24 21:29:11 +0200

    Remove ZoomControl component

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import EventsLanes from '../components/EventsLane';
-import ZoomControl from '../components/ZoomControl';
 import Sidebar from '../components/Sidebar';
 import './App.css';
 
 const App = () => (
 	<div className="appContainer">
 		<Sidebar />
-		<ZoomControl />
 		<EventsLanes />
 	</div>
 );
