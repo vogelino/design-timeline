a2608db4a1cc496b89d76873fe5640e03675358f
vogelino <contact@vogelino.com>
2017-06-27 23:26:07 +0200

    Remove usage of combineReducer in rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,16 +1,15 @@
-import { combineReducers } from 'redux';
 import events from './eventsReducer';
 import categories from './categoriesReducer';
 import zoom from './zoomReducer';
 import mouse from './mouseReducer';
 import ui from './uiReducer';
 
-const rootReducer = combineReducers({
-	events,
-	categories,
-	zoom,
-	mouse,
-	ui,
+const rootReducer = (state = {}, action = {}) => ({
+	events: events(state.events, action),
+	categories: categories(state.categories, action),
+	zoom: zoom(state.zoom, action),
+	mouse: mouse(state.mouse, action),
+	ui: ui(state.ui, action),
 });
 
 export default rootReducer;
